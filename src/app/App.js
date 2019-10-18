f50320e9e06cdc27f42122c81b53db3e19f80a9f
Мухутдинов Искандер <mi@smarthead.ru>
2019-10-18 11:21:39 +0300

    Настраивает структуру проекта.

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import HelloWorld from '../components/HelloWorld';
+
+const App = () => (
+    <div className="App">
+        <header className="App-header">
+            <HelloWorld />
+        </header>
+    </div>
+);
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloWorld = () => (
+    <div>
+        Всем привет.
+    </div>
+);
+
+export default HelloWorld;
